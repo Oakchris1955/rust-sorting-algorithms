b22ba22cc1b5e43e06403d8534497312d1e5f393
Oakchris1955 <80592203+Oakchris1955@users.noreply.github.com>
2023-05-14 16:59:59 +0300

    Implement tests

--- src/lib.rs
+++ src/lib.rs
@@ -18,3 +18,21 @@ pub fn insertion_sort(list: &mut Vec<usize>) {
 	}
 }
 
+#[cfg(test)]
+mod tests {
+	const UNSORTED_LIST: &[usize] = &[6, 3, 4, 2, 5, 1];
+	const SORTED_LIST: &[usize]= &[1, 2, 3, 4, 5, 6];
+
+    use super::*;
+	
+	fn general_test(list: &mut Vec<usize>, func: fn(&mut Vec<usize>)) {
+		func(list);
+
+		assert_eq!(list, SORTED_LIST, "Expected lists to be equal");
+	}
+
+    #[test]
+    fn insertion_test() {
+        general_test(&mut UNSORTED_LIST.to_vec(), insertion_sort);
+    }
+}
